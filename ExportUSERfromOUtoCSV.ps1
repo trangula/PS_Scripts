aa4286d11ed99ba8e6c4be61bca7dc9bd43d646e
trangula <52312283+trangula@users.noreply.github.com>
2019-07-01 09:37:05 +0200

    Add files via upload

---
+++ ExportUSERfromOUtoCSV.ps1
@@ -0,0 +1,4 @@
+﻿$OUpath = 'OU=Disabled accounts,OU=DSCEN,DC=DSCEN,DC=CZ'
+$ExportPath = 'C:\Export_OU_user.csv'
+Get-ADUser -Filter * -SearchBase $OUpath |
+Select-object SamAccountName,Name,UserPrincipalName | Export-Csv -NoTypeInformation -Encoding UTF8 -Delimiter ";" $ExportPath
