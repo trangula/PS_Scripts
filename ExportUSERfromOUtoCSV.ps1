6837fc2c0124be8b5e4eed70300cf614163c3bb1
trangula <52312283+trangula@users.noreply.github.com>
2019-07-01 09:44:52 +0200

    Update ExportUSERfromOUtoCSV.ps1

--- ExportUSERfromOUtoCSV.ps1
+++ ExportUSERfromOUtoCSV.ps1
@@ -1,4 +1,4 @@
-﻿$OUpath = 'OU=Disabled accounts,OU=DSCEN,DC=DSCEN,DC=CZ'
+﻿$OUpath = 'OU=Disabled accounts,OU=xxx,DC=xxx,DC=CZ'
 $ExportPath = 'C:\Export_OU_user.csv'
 Get-ADUser -Filter * -SearchBase $OUpath |
 Select-object SamAccountName,Name,UserPrincipalName | Export-Csv -NoTypeInformation -Encoding UTF8 -Delimiter ";" $ExportPath
